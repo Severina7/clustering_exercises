91657334d52b5e8c2acb66b8f8c2ee9c502caa5e
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-06-13 21:37:50 -0500

    adding exercises on  mall_customers file and axplore_zillow as well as continued exercises on zillow exercises

--- acquire.py
+++ acquire.py
@@ -3,39 +3,39 @@ import numpy as np
 import os
 from env import host, user, password
 
-# Acquiring the Zillow Data
+# # Acquiring the Zillow Data
 
-def get_connection(db, user=user, host=host, password=password):
-    '''
-    get_connection uses login info from env.py file to access Codeup db.
-    It takes in a string name of a database as an argument.
-    '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+# def get_connection(db, user=user, host=host, password=password):
+#     '''
+#     get_connection uses login info from env.py file to access Codeup db.
+#     It takes in a string name of a database as an argument.
+#     '''
+#     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 
-def get_zillow_data():
-    '''
-    zillow_data() gets the zillow (only properties_2017 table) data from Codeup db, then writes it to a csv file,
-    and returns the DF.
-    '''
-    # Creating a SQL query
-    sql_query = '''
-                SELECT 
-                       bedroomcnt,
-                       bathroomcnt,
-                       calculatedfinishedsquarefeet,
-                       taxvaluedollarcnt,
-                       propertylandusetypeid,
-                       transactiondate
-                FROM properties_2017
-                JOIN propertylandusetype USING(propertylandusetypeid)
-                JOIN predictions_2017 USING(parcelid)
-                WHERE propertylandusetypeid = '261'
-                '''
+# def get_zillow_data():
+#     '''
+#     zillow_data() gets the zillow (only properties_2017 table) data from Codeup db, then writes it to a csv file,
+#     and returns the DF.
+#     '''
+#     # Creating a SQL query
+#     sql_query = '''
+#                 SELECT 
+#                        bedroomcnt,
+#                        bathroomcnt,
+#                        calculatedfinishedsquarefeet,
+#                        taxvaluedollarcnt,
+#                        propertylandusetypeid,
+#                        transactiondate
+#                 FROM properties_2017
+#                 JOIN propertylandusetype USING(propertylandusetypeid)
+#                 JOIN predictions_2017 USING(parcelid)
+#                 WHERE propertylandusetypeid = '261'
+#                 '''
     
-    # Reading in the DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_connection('zillow'))
-    return df
+#     # Reading in the DataFrame from Codeup db.
+#     df = pd.read_sql(sql_query, get_connection('zillow'))
+#     return df
 
 
 def get_local_zillow():
@@ -48,12 +48,12 @@ def get_local_zillow():
         # If csv file exists read in data from csv file.
         df = pd.read_csv('properties_2017.csv', index_col=0)
         
-    else:
+    # else:
         
-        # Read fresh data from db into a DataFrame
-        df = get_zillow_data()
+    #     # Read fresh data from db into a DataFrame
+    #     df = get_zillow_data()
         
-        # Cache data
-        df.to_csv('properties_2017.csv')
+    #     # Cache data
+    #     df.to_csv('properties_2017.csv')
         
     return df
