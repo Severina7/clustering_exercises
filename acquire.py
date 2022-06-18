e74477858fa663292ba4bb04adf0248b45b8d7aa
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-06-17 19:07:19 -0500

    adding markdown changing to file

--- acquire.py
+++ acquire.py
@@ -57,3 +57,44 @@ def get_local_zillow():
     #     df.to_csv('properties_2017.csv')
         
     return df
+
+#     def get_zillow_data():
+#    '''
+#    Pulls in data from SQL server. 
+#    Only columns without large amounts of nulls where lat and long are not null
+#    And where bed and bath count are greater than 0
+#    ''' 
+#    query = '''
+#    SELECT
+#       properties_2017.parcelid,
+#       bathroomcnt,
+#       bedroomcnt,
+#       calculatedfinishedsquarefeet,
+#       fips,
+#       latitude,
+#       longitude,  
+#       lotsizesquarefeet,
+#       yearbuilt, 
+#       structuretaxvaluedollarcnt,
+#       taxvaluedollarcnt, 
+#       landtaxvaluedollarcnt,
+#       taxamount,                      
+#       predictions_2017.logerror,                       
+#       predictions_2017.transactiondate
+#    FROM properties_2017
+#    JOIN predictions_2017 USING(parcelid)
+#    LEFT JOIN airconditioningtype USING(airconditioningtypeid)
+#    LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)
+#    LEFT JOIN buildingclasstype USING(buildingclasstypeid)
+#    LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)
+#    LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+#    LEFT JOIN storytype USING(storytypeid)
+#    LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)
+#    WHERE
+#       latitude IS NOT NULL
+#       AND longitude IS NOT NULL
+#    AND bathroomcnt > 0
+#    AND bedroomcnt > 0
+#    '''
+#    df = pd.read_sql(query, get_db_url('zillow'))
+#    return df
